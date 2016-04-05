cc38ce3e1e2899d86245c81f08b0f6ecb052c5b4
Bill Cauchois <wcauchois@gmail.com>
2016-04-04 22:35:05 -0400

    work2

--- app.js
+++ app.js
@@ -64,6 +64,15 @@ function clearScreen() {
   screenBuf.fill(0);
 }
 
+function drawPixel(x, y, color) {
+  x = Math.floor(x);
+  y = Math.floor(y);
+  screenBuf[(x + y * 64) * 4 + 0] = color[0];
+  screenBuf[(x + y * 64) * 4 + 1] = color[1];
+  screenBuf[(x + y * 64) * 4 + 2] = color[2];
+  screenBuf[(x + y * 64) * 4 + 3] = (color.length > 3) ? color[3] : 255;
+}
+
 function drawImage(startX, startY, obj, clearColor) {
   var w = obj.width, h = obj.height;
   startX = Math.floor(startX);
@@ -75,39 +84,41 @@ function drawImage(startX, startY, obj, clearColor) {
           b = obj.data[(x + y * w) * 3 + 1];
       var alpha = 255;
       if (
-        typeof clearColor !== 'undefined' &&
-        r === clearColor[0] &&
-        g === clearColor[1] &&
-        b === clearColor[2]
+        typeof clearColor === 'undefined' ||
+        r !== clearColor[0] ||
+        g !== clearColor[1] ||
+        b !== clearColor[2]
       ) {
-        alpha = 0;
+        drawPixel(x + startX, y + startY, [r, g, b]);
       }
-      screenBuf[((x + startX) + (y + startY) * 64) * 4 + 0] = r;
-      screenBuf[((x + startX) + (y + startY) * 64) * 4 + 1] = g;
-      screenBuf[((x + startX) + (y + startY) * 64) * 4 + 2] = b;
-      screenBuf[((x + startX) + (y + startY) * 64) * 4 + 3] = alpha;
     }
   }
 }
 
 var K_UP = 38, K_DOWN = 40, K_LEFT = 37, K_RIGHT = 39;
+var K_SPACE = 32;
 var moveMap = {};
 moveMap[K_UP] =  vec2.fromValues(0, -1);
 moveMap[K_DOWN] = vec2.fromValues(0, 1);
 moveMap[K_LEFT] = vec2.fromValues(-1, 0);
 moveMap[K_RIGHT] = vec2.fromValues(1, 0);
 var keysDown = {};
+var capturedKeys = [K_UP, K_DOWN, K_LEFT, K_RIGHT, K_SPACE];
+
+var C_WHITE = [255, 255, 255],
+    C_BEAM_OUTER = [90, 239, 255],
+    C_BEAM_INNER = [247, 255, 252];
 
 getJSON('ships.json').then(function(json) {
   window.addEventListener('keydown', function(e) {
-    if (e.keyCode in moveMap) {
+    if (capturedKeys.indexOf(e.keyCode) >= 0) {
       e.preventDefault();
       keysDown[e.keyCode] = true;
     }
   });
 
   window.addEventListener('keyup', function(e) {
-    if (e.keyCode in moveMap) {
+    if (capturedKeys.indexOf(e.keyCode) >= 0) {
       e.preventDefault();
       keysDown[e.keyCode] = false;
     }
@@ -120,21 +131,26 @@ getJSON('ships.json').then(function(json) {
   var SHIP_ACCEL = 0.3;
   var SHIP_EPS = 0.0001;
 
+  var firing = false;
+
   setInterval(function() {
     var indices = Object.keys(keysDown);
-    var keys = indices.filter(function(i) { return keysDown[i]; });
-    keys.forEach(function(k) {
-      var dir = moveMap[k];
-      if (dir) {
-        vec2.scaleAndAdd(shipVel, shipVel, dir, SHIP_ACCEL);
-      }
+    var movementKeys = indices.filter(function(k) { return keysDown[k] && (k in moveMap); });
+    movementKeys.forEach(function(k) {
+      vec2.scaleAndAdd(shipVel, shipVel, moveMap[k], SHIP_ACCEL);
     });
     if (Math.abs(shipVel[0]) < SHIP_EPS && Math.abs(shipVel[1]) < SHIP_EPS) {
       vec2.set(shipVel, 0, 0);
     }
     vec2.add(shipPos, shipPos, shipVel);
     vec2.scale(shipVel, shipVel, SHIP_FRICTION);
+
+    firing = !!keysDown[K_SPACE];
   }, 50);
+
+  var kamehaOffset = vec2.fromValues(17, 2);
+  var beamOffset = vec2.add(vec2.create(), kamehaOffset, vec2.fromValues(10, 3));
+  var beamHeight = 4;
 
   function animate() {
     var imageToUse;
@@ -147,7 +163,23 @@ getJSON('ships.json').then(function(json) {
     }
 
     clearScreen();
-    drawImage(shipPos[0], shipPos[1], imageToUse, [255, 255, 255]);
+
+    if (firing) {
+      var kamehaPos = vec2.add(vec2.create(), shipPos, kamehaOffset);
+      drawImage(kamehaPos[0], kamehaPos[1], json.kameha, C_WHITE);
+    }
+
+    drawImage(shipPos[0], shipPos[1], imageToUse, C_WHITE);
+
+    if (firing) {
+      for (var x = shipPos[0] + beamOffset[0]; x < 64; x++) {
+        for (var y = 0; y < beamHeight; y++) {
+          var color = (y === 0 || y === (beamHeight - 1)) ? C_BEAM_OUTER : C_BEAM_INNER;
+          drawPixel(x, y + shipPos[1] + beamOffset[1], color);
+        }
+      }
+    }
+
     renderScreen();
     requestAnimationFrame(animate);
   }
