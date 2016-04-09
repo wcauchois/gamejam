7fa618e5b39fe8664f654a2036c129e9bc0a7a10
Bill Cauchois <wcauchois@gmail.com>
2016-04-09 13:10:37 -0400

    cleanup/xml loading

--- src/Utils.js
+++ src/Utils.js
@@ -12,21 +12,13 @@ exports.loadImage = function(url) {
   });
 };
 
-exports.getJSON = function(url) {
+var runXHR = function(url, method) {
   return new Promise(function(resolve, reject) {
     var xhr = new XMLHttpRequest();
-    xhr.open('GET', url);
+    xhr.open(method, url);
     xhr.onload = function() {
       if (this.status >= 200 && this.status < 300) {
-        var json;
-        try {
-          json = JSON.parse(xhr.responseText);
-        } catch (e) {}
-        if (json) {
-          resolve(json);
-        } else {
-          reject(new Error("Server returned invalid JSON"));
-        }
+        resolve(xhr);
       } else {
         reject(new Error("Server responded with status " + xhr.status + " " + xhr.statusText));
       }
@@ -37,4 +29,28 @@ exports.getJSON = function(url) {
     xhr.send();
   });
 };
+
+exports.getXML = function(url) {
+  return runXHR(url, 'GET').then(function(xhr) {
+    return xhr.responseXML;
+  });
+};
+
+exports.getJSON = function(url) {
+  return runXHR(url, 'GET').then(function(xhr) {
+    return new Promise(function(resolve, reject) {
+      var json;
+      try {
+        json = JSON.parse(xhr.responseText);
+      } catch (e) {}
+      if (json) {
+        resolve(json);
+      } else {
+        reject(new Error("Server returned invalid JSON"));
+      }
+    });
+  });
+};
+
+global.Utils = module.exports;
 
