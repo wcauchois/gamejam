ec5fcbeea2afda7940222a679353935c2bd24d5c
Bill Cauchois <wcauchois@gmail.com>
2016-04-12 22:58:06 -0400

    more efficient star field maybe

--- src/StarField.js
+++ src/StarField.js
@@ -4,8 +4,6 @@ var GameObject = require('./GameObject'),
     GameTime = require('./GameTime'),
     Utils = require('./Utils');
 
-var starColor = 'rgb(255, 255, 255)';
-
 var NUM_STARS = 13;
 var FIELD_SPEED = 0.02;
 
@@ -23,15 +21,15 @@ var Star = Base.extend({
   _resetColor: function() {
     var r = Math.random();
     if (r < 0.05) {
-      this._color = 'rgba(0, 0, 0, 0)';
+      this._color = [0, 0, 0, 0];
     } else if (r < 0.2875) {
-      this._color = '#9E9E9E';
+      this._color = [158, 158, 158, 255];
     } else if (r < 0.525) {
-      this._color = '#A0A0B3';
+      this._color = [160, 160, 179, 255];
     } else if (r < 0.7625) {
-      this._color = '#FFFFFD';
+      this._color = [255, 255, 253, 255];
     } else {
-      this._color = '#DDDDDD';
+      this._color = [221, 221, 221, 255];
     }
   },
 
@@ -41,8 +39,8 @@ var Star = Base.extend({
     this._resetColor();
     this._speed = Utils.randIntBetween(10, 15);
   },
-  getX: function() { return this._pos[0]; },
-  getY: function() { return this._pos[1]; }
+  getX: function() { return Math.floor(this._pos[0]); },
+  getY: function() { return Math.floor(this._pos[1]); }
 });
 
 var StarField = GameObject.extend({
@@ -69,9 +67,16 @@ var StarField = GameObject.extend({
   },
 
   draw: function(ctx) {
+    var imageData = ctx.createImageData(64, 64);
     this._stars.forEach(function(star) {
-      Utils.fillRect(ctx, star.getColor(), star.getX(), star.getY(), 1, 1);
+      var color = star.getColor();
+      var baseIndex = (star.getX() + star.getY() * 64) * 4;
+      imageData.data[baseIndex + 0] = color[0];
+      imageData.data[baseIndex + 1] = color[1];
+      imageData.data[baseIndex + 2] = color[2];
+      imageData.data[baseIndex + 3] = color[3];
     });
+    ctx.putImageData(imageData, 0, 0);
   }
 });
 
